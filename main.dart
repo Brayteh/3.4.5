20ce66f956ecbf6a7539509a8f8ef383ee472501
Brayteh <ya3koub5@gmail.com>
2025-06-13 10:37:35 +0200

    Aufg 3.4.5(1)

---
+++ main.dart
@@ -0,0 +1,6 @@
+void main () {
+    List<String> names = ['Julietta', 'Benjamino', 'Hans-Günther','Evalinea', 'Fiona', 'Gregory', 'Leonhart'];
+     
+     for (String Name in names){ print(Name);}
+
+}

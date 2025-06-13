dcc698f6b725bbf6010345462d3fc3f6b4a95587
Brayteh <ya3koub5@gmail.com>
2025-06-13 11:06:31 +0200

    aufg 3.4.5(2)

--- main.dart
+++ main.dart
@@ -1,6 +1,16 @@
 void main () {
+    List<int> points = [4, 5, 4, 2, 6, 6, 3];
     List<String> names = ['Julietta', 'Benjamino', 'Hans-Günther','Evalinea', 'Fiona', 'Gregory', 'Leonhart'];
      
      for (String Name in names){ print(Name);}
+
+     int sumPoints = 0;
+     for (int point in points) { sumPoints += point;}
+    print("Total Punkte : $sumPoints");
+
+    double avrgPunkte = sumPoints / points.length;
+    print("Durschnittliche Punkte : $avrgPunkte");
+
+
 
 }
